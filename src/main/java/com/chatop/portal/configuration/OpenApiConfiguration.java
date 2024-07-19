466cfc378544e07b4867da1abc1afc6ecab95d6a
oulibouli <oulikhanow.nico@gmail.com>
2024-07-19 15:06:36 +0200

    Implement spring security

--- src/main/java/com/chatop/portal/configuration/OpenApiConfiguration.java
+++ src/main/java/com/chatop/portal/configuration/OpenApiConfiguration.java
@@ -1,4 +1,4 @@
-package com.chatop.portal;
+package com.chatop.portal.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
