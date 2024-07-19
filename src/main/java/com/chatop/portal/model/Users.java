12ad7157c91c5f20103092cdf925e6df1749e965
oulibouli <oulikhanow.nico@gmail.com>
2024-07-19 15:08:04 +0200

    Create Users model

---
+++ src/main/java/com/chatop/portal/model/Users.java
@@ -0,0 +1,69 @@
+package com.chatop.portal.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Users {
+    @Id // Annotation defining the unique identifier
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String name;
+    private String password;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+
+    // Defining the getters / setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
+}
